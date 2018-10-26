63cb25b66466d8991805c81f8a05140c4767076d
Rommel Rico <me@rommelrico.com>
2018-10-25 18:45:43 -0700

    Initial commit

---
+++ public/javascripts/chatmeter.js
@@ -0,0 +1,17 @@
+// File for handling ChatMeter challenge.
+
+// this is the id of the form
+// $("#urlSubmissionForm").submit(function(e) {
+//
+//     var form = $(this);
+//     var url = form.attr('action');
+//
+//     $.ajax({
+//         type: "POST",
+//         url: url,
+//         data: form.serialize(), // serializes the form's elements.
+//         success: function(data) {
+//             console.log(data);
+//         }
+//     });
+// });
